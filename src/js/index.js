c1860ea3e6216f0c50779a73ca910323aa540c53
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-12 22:10:49 +0300

    added card

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import '../css/styles.css';
 
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
-const infotEl = document.querySelector('.country-info');
+const infoEl = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 const INFO_TEXT = 'Too many matches found. Please enter a more specific name.';
@@ -16,7 +16,7 @@ inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 function onInputChange({ target: { value } }) {
   if (value.trim().length === 0) {
     listEl.innerHTML = '';
-    infotEl.innerHTML = '';
+    infoEl.innerHTML = '';
     return console.log('empty query');
   }
 
@@ -25,8 +25,11 @@ function onInputChange({ target: { value } }) {
       if (data.status === 404) throw new Error('No data');
       if (data.length > 10) return Notify.info(INFO_TEXT);
 
-      console.log(data);
-      return 'markup';
+      if (data.length === 1) return createCard(data[0]);
+      createList(data);
+
+      // console.log(data);
+      // return 'markup';
     })
     // .then(renderMarkup)
     .catch(_err => {
@@ -35,14 +38,38 @@ function onInputChange({ target: { value } }) {
     });
 }
 
-function createList(counries) {
-  //
+function createList(countries) {
+  console.log(countries);
+  const listMarkup = createListMarkup(countries);
+  infoEl.innerHTML = '';
+  listEl.innerHTML = listMarkup;
 }
 
-function crateCard(country) {
-  //
+function createCard(country) {
+  console.log(country);
+  const cardMarkup = createCardMarkup(country);
+  listEl.innerHTML = '';
+  infoEl.innerHTML = cardMarkup;
 }
 
-function renderMarkup(markup) {
+function createCardMarkup(country) {
+  const { capital, population, name, languages, flags } = country;
+  const languageList = Object.values(languages);
+  return `<h2 class="country__name">
+            <img
+              class="country__flag"
+              width='35'
+              src=${flags.svg}
+              alt=${flags.alt}
+            >
+            ${name.official}
+          </h2>
+          <p>Capital: ${capital}</p>
+          <p>Population: ${population}</p>
+          <p>Languages: ${languageList.join(', ')}</p>`;
+}
+
+function createListMarkup(countries) {
+  console.log(countries);
   //
 }
